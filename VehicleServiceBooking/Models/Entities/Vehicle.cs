b2610ff1181680a4361289e99a994ad740319500
Lenti19 <lenti.tahiri777@gmail.com>
2026-01-27 14:08:51 +0100

    krijimi i vehicle.cs tek entities dhe implementimi i modulit Vehicle Managment

---
+++ VehicleServiceBooking/Models/Entities/Vehicle.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VehicleServiceBooking.Web.Models.Entities;
+
+public class Vehicle
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string LicensePlate { get; set; } = null!;
+
+    [Required]
+    public string Brand { get; set; } = null!;
+
+    [Required]
+    public string Model { get; set; } = null!;
+
+    public int Year { get; set; }
+
+    
+    public string? ClientId { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+}
