2eaf5159a797f008f50634ee9d3991d36ad8e052
Lenti19 <lenti.tahiri777@gmail.com>
2026-01-29 23:46:05 +0100

    inicializimi i te dhenave baze ne start

---
+++ VehicleServiceBooking/Data/Seed/DbInitializer.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using VehicleServiceBooking.Web.Models.Entities;
+
+namespace VehicleServiceBooking.Web.Data.Seed;
+
+public static class DbInitializer
+{
+    public const string ManagerRole = "Manager";
+    public const string MechanicRole = "Mechanic";
+    public const string ClientRole = "Client";
+
+    public static async Task InitializeAsync(IServiceProvider serviceProvider)
+    {
+        using var scope = serviceProvider.CreateScope();
+
+        var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+        var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+        try
+        {
+            if (context.Database.CanConnect())
+            {
+                await context.Database.MigrateAsync();
+            }
+        }
+        catch
+        {
+        }
+
+        await SeedRolesAsync(roleManager);
+        await SeedManagerUserAsync(userManager);
+    }
+
+    private static async Task SeedRolesAsync(RoleManager<IdentityRole> roleManager)
+    {
+        string[] roles = { ManagerRole, MechanicRole, ClientRole };
+
+        foreach (var role in roles)
+        {
+            if (!await roleManager.RoleExistsAsync(role))
+            {
+                await roleManager.CreateAsync(new IdentityRole(role));
+            }
+        }
+    }
+
+    private static async Task SeedManagerUserAsync(UserManager<ApplicationUser> userManager)
+    {
+        const string managerEmail = "manager@vehicleservice.com";
+        const string managerPassword = "Manager@123";
+
+        var manager = await userManager.FindByEmailAsync(managerEmail);
+        if (manager == null)
+        {
+            manager = new ApplicationUser
+            {
+                UserName = managerEmail,
+                Email = managerEmail,
+                EmailConfirmed = true,
+                FirstName = "System",
+                LastName = "Manager",
+                IsActive = true,
+                CreatedAt = DateTime.UtcNow
+            };
+
+            var result = await userManager.CreateAsync(manager, managerPassword);
+            if (result.Succeeded)
+            {
+                await userManager.AddToRoleAsync(manager, ManagerRole);
+            }
+        }
+    }
+}
