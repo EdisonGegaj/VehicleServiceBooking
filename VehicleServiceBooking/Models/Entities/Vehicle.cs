d6d2776a8b94d64bc6da6053fd29ba395961c709
EdisonGegaj <edisongegaj10@gmail.com>
2026-01-30 12:48:15 +0100

    Vehcile Permisimi

--- VehicleServiceBooking/Models/Entities/Vehicle.cs
+++ VehicleServiceBooking/Models/Entities/Vehicle.cs
@@ -1,24 +1,46 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace VehicleServiceBooking.Web.Models.Entities;
 
 public class Vehicle
 {
+    [Key]
     public int Id { get; set; }
 
     [Required]
-    public string LicensePlate { get; set; } = null!;
+    [StringLength(50)]
+    public string Make { get; set; } = string.Empty;
 
     [Required]
-    public string Brand { get; set; } = null!;
+    [StringLength(50)]
+    public string Model { get; set; } = string.Empty;
 
     [Required]
-    public string Model { get; set; } = null!;
+    [StringLength(20)]
+    public string LicensePlate { get; set; } = string.Empty;
 
-    public int Year { get; set; }
+    [Range(1900, 2100, ErrorMessage = "Year must be between 1900 and 2100")]
+    public int? Year { get; set; }
 
-    
-    public string? ClientId { get; set; }
+    [StringLength(17)]
+    public string? VIN { get; set; }
 
-    public DateTime CreatedAt { get; set; }
+    [StringLength(50)]
+    public string? Color { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    [Required]
+    public string ClientId { get; set; } = string.Empty;
+
+    [JsonIgnore]
+    [ForeignKey(nameof(ClientId))]
+    public virtual ApplicationUser? Client { get; set; }
+
+    [JsonIgnore]
+    public virtual ICollection<Booking> Bookings { get; set; } = new List<Booking>();
 }
+
+
